0160fb21f331235c93fee225576f967b32eefd3b
cheoklate <gcheok88@gmail.com>
2022-04-26 03:28:03 +0800

    models

---
+++ models/users.mjs
@@ -0,0 +1,28 @@
+export default function personModel(sequelize, DataTypes) {
+    return sequelize.define('person', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+      admin: {
+        type: DataTypes.BOOL,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    }, { underscored: true });
+  }
+  
