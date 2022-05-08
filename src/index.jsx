a682afbf01f529115a2091336065137091c24208
cheoklate <gcheok88@gmail.com>
2022-05-08 21:22:04 +0800

    added backdrop

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,9 @@ import React, { lazy, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Switch } from 'react-router-dom';
 import './styles.scss';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+
 
 const Homepage = lazy(() => import('./components/Blog/Blog.jsx'));
 const SignUpPage = lazy(() => import('./components/SignUp.jsx'));
@@ -16,7 +19,18 @@ const root = createRoot(rootElement);
 
 root.render(
 	<BrowserRouter>
-		<Suspense fallback = {<div>Loading Page...</div>}>
+		<Suspense
+			fallback={
+				<div>
+					<Backdrop
+						sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+						open={open}
+					>
+						<CircularProgress color='inherit' />
+					</Backdrop>
+				</div>
+			}
+		>
 			<Routes>
 				<Route path='/' element={<Homepage />} />
 				<Route path='signup' element={<SignUpPage />} />
